6415390f89c5ba32450dcc2f33e41573a1ec8464
moslem gouia <moslem.gouia@gmail.com>
2025-08-06 22:23:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/map/interactive-map.tsx
+++ src/components/map/interactive-map.tsx
@@ -4,6 +4,20 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import type { LocationWithCoords } from '@/lib/types';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import { useEffect } from 'react';
+
+// Fix for marker icon issue with webpack
+// This is a common workaround for react-leaflet
+if (typeof window !== 'undefined') {
+    delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+      iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+      shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    });
+}
 
 interface InteractiveMapProps {
     locations: LocationWithCoords[];
